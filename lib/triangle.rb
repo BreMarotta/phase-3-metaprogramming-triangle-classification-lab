7e1b8bca1c522397b692e594ea87b6e064d85e1f
Breanne <BreMarotta@gmail.com>
2022-03-09 09:47:20 -0500

    commit tree 5f749774a19397fe251f07318194c505a31b10de

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_reader :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+  
+  def kind 
+    validate_triangle
+    if side_1 == side_2 && side_2 == side_3
+      :equilateral
+    elsif side_1 == side_2 || side_2 == side_3 || side_3 == side_1
+    :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_valid? 
+    [side_1, side_2, side_3].all?(&:positive?)
+  end
+
+  def valid_triangle? 
+    side_1 + side_2 > side_3 && side_2 + side_3 > side_1 && side_3 + side_1 > side_2
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_valid? && valid_triangle?
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
